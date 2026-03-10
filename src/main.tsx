d2bfc3beacb1064d83f1c2d02f0ea34b270545ef
Jookman1 <admin@jackwoodall.com>
2026-03-10 03:40:29 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home as KairoSecHome } from './pages/Home.tsx';
 import App from './App.tsx';
-import { KairoClean } from './pages/KairoClean.tsx';
+import KairoClean from './pages/KairoClean.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
